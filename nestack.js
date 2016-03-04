8aa80d103da53724853088ff1cb8d9bfb9298827
Benjamin Pang <molarmanful@users.noreply.github.com>
2016-03-03 22:05:11 -0800

    Update nestack.js

--- nestack.js
+++ nestack.js
@@ -20,6 +20,9 @@ braces={
 put=s=>out.textContent+=s
 getc=_=>ini<inp.value.length?inp.value.charCodeAt(ini++):-1
 commands={
+  "\0":x=>cur.push(cur.length),
+  "\1":x=>cur=cur==stack1?stack2:stack1,
+  "{":x=>cur=cur[cur.length-1].pop?cur[cur.length-1]:[cur[cur.length-1]],
 	"%":x=>cur.pop(),
 	"$":x=>cur.push(cur.pick(0)),
 	"^":x=>cur.push(cur.pick(1)),
@@ -36,19 +39,22 @@ commands={
 	"&":x=>cur.push(cur.pop()&cur.pop()),
 	"|":x=>cur.push(cur.pop()^cur.pop()),
 	"~":x=>cur.push(~cur.pop()),
+	"¤":x=>Math.sign(cur.pop()),
+	"<":x=>cur.push(-(cur.pop()>cur.pop())),
+	">":x=>cur.push(-(cur.pop()<cur.pop())) },
 	"'":x=>cur.push(code.charCodeAt(++ip)),
-	'"':x=>{for(;code[++ip]!='"';)cur[cur.length-1].push(code.charCodeAt(ip));cur.push(i)},
+	'"':x=>{for(i=0;code[++ip]!='"';i++)cur[cur.length-1].push(code.charCodeAt(ip));cur.push(i)},
 	".":x=>put(cur.pop()),
 	",":x=>put(String.fromCharCode(cur.pop())),
 	"`":x=>cur.push(getc()),
 	":":x=>vars[cur.pop()]=cur.pop(),
 	";":x=>cur.push(vars[cur.pop()]),
 	"[":x=>(cur.push(ip),ip=matching_brace()),
-	"]":{x=>n=ret.length-3;if(~n&&code[ret[n]]=='#'){if(cur.pop())ret.push(ret[n+1],ret[n+2]);else ret.pop(),ret.pop()}ip=ret.pop()},
+	"]":x=>{n=ret.length-3;if(~n&&code[ret[n]]=='#'){if(cur.pop())ret.push(ret[n+1],ret[n+2]);else ret.pop(),ret.pop()}ip=ret.pop()},
 	"!":x=>(ret.push(ip),ip=cur.pop()),
 	"?":x=>(f=cur.pop(),t=cur.pop(),ret.push(ip),ip=cur.pop()?t:f),
 	"#":x=>(ret.push(ip,cur.pick(1),cur.pop()),ip=cur.pop()),
-	"⇒":x=>(op=cur.pop(),commands[code[++ip]]=x=>ret.push(ip),ip=op)
+	"=":x=>(op=cur.pop(),commands[code[++ip]]=x=>ret.push(ip),ip=op)
 }
 eval=_=>{
 	c=code[ip]
@@ -56,6 +62,9 @@ eval=_=>{
 	else if(/\d/.test(c)){num=code.substring(ip).match(/\d+/)[0];cur.push(+num);ip+=num.length;return}
 	else if(/\s/.test(c)){ip+=code.substring(ip).match(/\s+/)[0].length;return}
 	else cur.push(c);ip++
+	console.log(`Stack1: ${JSON.stringify(stack1)}
+Stack2: ${JSON.stringify(stack2)}
+Return Stack: ${JSON.stringify(ret)}`)
 }
 init=_=>(ahead=[],ip=0,ret=[],stack1=[],stack2=[],cur=stack1,vars={},ini=0,out.innerHTML="")
-run=_=>{ip>=code.length&&init();for(;ip<code.length;)eval()}
+run=_=>{code=c.value,ip>=code.length&&init();for(;ip<code.length;)eval()}
