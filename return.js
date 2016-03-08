5e27d1d61ac82472bf7cfd62528bee4767b72cc1
Benjamin Pang <molarmanful@users.noreply.github.com>
2016-03-07 22:01:52 -0800

    Update return.js

--- return.js
+++ return.js
@@ -83,7 +83,7 @@ commands = {
 		return cur.chunk(cur.pop());
 	},
 	"\n": function _(x) {
-		return a = cur.pop(), b = cur.pop(), cur.push(math.range(Math.min(a, b), Math.max(a, b)));
+		return a = cur.pop(), b = cur.pop(), cur.push(math.range(Math.min(a, b), Math.max(a, b))._data);
 	},
 	"\v": function _(x) {},
 	"\f": function _(x) {},
