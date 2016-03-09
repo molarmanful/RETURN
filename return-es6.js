be2e2c2318cda2566978fd3f1710d2c312748f8b
Benjamin Pang <molarmanful@users.noreply.github.com>
2016-03-08 19:34:11 -0800

    Update return-es6.js

--- return-es6.js
+++ return-es6.js
@@ -39,7 +39,7 @@ commands={
 	"\t":x=>cur=cur.chunk(cur.pop()),
 	"\n":x=>(a=cur.pop(),b=cur.pop(),cur.push(math.range(Math.min(a,b),Math.max(a,b))._data)),
 	"\v":x=>cur=(a=[],[...Array(3)].map(x=>a=[a,...cur]),cur=a),
-	"\f":x=>{},
+	"\f":x=>cur=cur.filter(x=>x),
 	"{":x=>(cur=cur[cur.length-1].pop?cur[x=cur.length-1]:(cur[cur.length-1]=[cur[x=cur.length-1]]),nest.push(x)),
 	"}":x=>{nest.length&&(nest.pop(),cur=curstack,nest.map(x=>cur=cur[x]))},
 	"%":x=>cur.pop(),
