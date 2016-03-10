d5b566731bba13f208b6c72bfae6af0992e92863
Benjamin Pang <molarmanful@users.noreply.github.com>
2016-03-09 18:21:09 -0800

    Update return.js

--- return.js
+++ return.js
@@ -102,7 +102,7 @@ commands = {
 
 		a = [], ip++;cur.slice(0).map(function (x) {
 			return commands[code[ip]](), a.unshift(cur.pop());
-		});cur = (_cur = cur).push.apply(_cur, _toConsumableArray(a));
+		});(_cur = cur).push.apply(_cur, _toConsumableArray(a));
 	}, //TODO: Better each function
 	"{": function _(x) {
 		return cur = cur[cur.length - 1].pop ? cur[x = cur.length - 1] : cur[cur.length - 1] = [cur[x = cur.length - 1]], nest.push(x);
