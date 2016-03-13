649f751e4ebc7f6927db25cd84b53ad981031595
Benjamin Pang <molarmanful@users.noreply.github.com>
2016-03-13 15:21:55 -0700

    Update return.js

--- return.js
+++ return.js
@@ -251,7 +251,7 @@ commands = {
 log = function log(_) {
 	return stats.innerHTML = 'Code          │ ' + (format = [].concat(_toConsumableArray(code.replace(/[\x00-\x1f]/g, function (x) {
 		return String.fromCharCode(x.charCodeAt() + 9216);
-	}))), format[ip - 1] = '<span style=background-color:#7ec0ee>' + code[ip - 1] + '</span>', format.join(_templateObject)) + '\nIP            │ ' + (ip-1) + '\nStack1        │ ' + JSON.stringify(stack1) + '\nStack2        │ ' + JSON.stringify(stack2) + '\nCurrent Stack │ ' + JSON.stringify(cur) + '\nNest Indices  │ ' + nest + '\nVariables     │ ' + JSON.stringify(vars) + '\nReturn Stack  │ ' + JSON.stringify(ret);
+	}))), format[ip] = '<span style=background-color:#7ec0ee>' + code[ip] + '</span>', format.join(_templateObject)) + '\nIP            │ ' + ip + '\nStack1        │ ' + JSON.stringify(stack1) + '\nStack2        │ ' + JSON.stringify(stack2) + '\nCurrent Stack │ ' + JSON.stringify(cur) + '\nNest Indices  │ ' + nest + '\nVariables     │ ' + JSON.stringify(vars) + '\nReturn Stack  │ ' + JSON.stringify(ret);
 };
 nsc.oninput = onload = function onload(_) {
 	return code = nsc.value, log();
@@ -260,11 +260,12 @@ nsc.oninput = onload = function onload(_) {
 //actual parsing
 parse = function parse(_) {
 	c = code[ip];
+	log();
 	if (commands[c]) commands[c]();else if (/\d/.test(c)) {
-		c = num = code.substring(ip).match(/\d+/)[0];cur.push(+num);ip += num.length;log();return;
+		c = num = code.substring(ip).match(/\d+/)[0];cur.push(+num);ip += num.length;return;
 	} else if (/\s/.test(c)) {
-		ip += (c = code.substring(ip).match(/\s+/)[0]).length;log();return;
-	} else cur.push(c);ip++;log();
+		ip += (c = code.substring(ip).match(/\s+/)[0]).length;return;
+	} else cur.push(c);ip++;
 };
 
 //clearing everything before starting prog
